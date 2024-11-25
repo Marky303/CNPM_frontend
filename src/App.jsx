49bc3ee23d29cae016c2ed7af696425cddc4245d
khoa0402 <165537223+khoa0402@users.noreply.github.com>
2024-11-25 20:25:30 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Import contexts
-import { UserauthProvider } from "./context/UserauthContext";
-
-// Import private route/link check
-import PrivateRouteCheck from "./hocs/PrivateRouteCheck";
-
-// Import hocs
-import Layout from "./hocs/Layout";
-import ChatBubbleLayout from "./hocs/ChatBubbleLayout";
-
-// Import main pages
-import Home from "./pages/Home";
-
-// Import introduction pages
-import Features from "./pages/Intro/Features";
-import Pricing from "./pages/Intro/Pricing";
-
-// Import authentication pages
-import Login from "./pages/Auth/Login";
-import Signup from "./pages/Auth/Signup";
-
-// Import main pages
-import Dashboard from "./pages/Main/Dashboard";
-
-// Import special pages
-import PageNotFound from "./pages/Error/PageNotFound";
+import Setting from "./components/Setting/Setting.jsx"
+import Report from "./components/Report/Report.jsx"
 
 // Import bootstrap css
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -33,21 +8,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const App = () => (
   <BrowserRouter>
     <Routes>
-      <Route exact path="/" element={<UserauthProvider />}>
-        <Route exact path="/" element={<PrivateRouteCheck />}>
-          <Route exact path="/" element={<Layout />}>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/features" element={<Features />} />
-            <Route exact path="/pricing" element={<Pricing />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/signup" element={<Signup />} />
-
-            <Route exact path="/" element={<ChatBubbleLayout />}>
-              <Route exact path="/dashboard" element={<Dashboard />} />
-            </Route>
-          </Route>
-        </Route>
-        <Route exact path="*" element={<PageNotFound />} />
+      <Route exact path="/" element={<Report/>}>
+        
       </Route>
     </Routes>
   </BrowserRouter>
